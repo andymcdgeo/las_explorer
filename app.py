a72e24ec283d4b25cc508e678c52e810bec3e682
Andy <andymcdonald1985@gmail.com>
2020-12-30 15:41:28 +0000

    Code Tidy Up

--- app.py
+++ app.py
@@ -6,26 +6,19 @@ import lasio
 import missingno as mno
 from multiapp import MultiApp
 import pandas as pd
+
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
-
 import plotly.express as px
 
 local_css("style.css")
 
-
 @st.cache
 def load_data(uploadedfile):
     if uploadedfile:
-        # uploaded_file = lasio.read(las_file_object)
-        
         string = uploadedfile.read().decode()
-        number = len(string)
-        # st.write(string)
-
         las_file = lasio.read(string)
-
         return las_file
 
 def home():
@@ -63,76 +56,74 @@ def raw_data():
 def plot():
     st.title('LAS File Visualisation')
     st.write('Expand one of the following to visualise your well data.')
-    columns = list(well_data.columns)
+    if not las_file:
+        st.warning('No file has been uploaded')
+    
+    else:
+        columns = list(well_data.columns)
 
-    #Log plot section
-    with st.beta_expander('Log Plot'):    
-        curves = st.multiselect('Select Curves To Plot', columns)
-        if len(curves) <= 1:
-            st.warning('Please select at least 2 curves.')
-        else:
-            curve_index = 1
-            fig = make_subplots(rows=1, cols= len(curves), subplot_titles=curves, shared_yaxes=True)
+        with st.beta_expander('Log Plot'):    
+            curves = st.multiselect('Select Curves To Plot', columns)
+            if len(curves) <= 1:
+                st.warning('Please select at least 2 curves.')
+            else:
+                curve_index = 1
+                fig = make_subplots(rows=1, cols= len(curves), subplot_titles=curves, shared_yaxes=True)
 
-            for curve in curves:
-                fig.add_trace(go.Scatter(x=well_data[curve], y=well_data['DEPTH']), row=1, col=curve_index)
-                curve_index+=1
+                for curve in curves:
+                    fig.add_trace(go.Scatter(x=well_data[curve], y=well_data['DEPTH']), row=1, col=curve_index)
+                    curve_index+=1
+                
+                fig.update_layout(height=1000, showlegend=False, yaxis={'title':'DEPTH','autorange':'reversed'})
+                fig.layout.template='seaborn'
+                st.plotly_chart(fig, use_container_width=True)
+
+        with st.beta_expander('Histograms'):
+            col1_h, col2_h = st.beta_columns(2)
+            col1_h.header('Options')
+
+            hist_curve = col1_h.selectbox('Select a Curve', columns)
+            log_option = col1_h.radio('Select Linear or Logarithmic Scale', ('Linear', 'Logarithmic'))
             
-            fig.update_layout(height=1000, showlegend=False, yaxis={'title':'DEPTH','autorange':'reversed'})
-            fig.layout.template='seaborn'
-            st.plotly_chart(fig, use_container_width=True)
+            if log_option == 'Linear':
+                log_bool = False
+            elif log_option == 'Logarithmic':
+                log_bool = True
+            
+            histogram = px.histogram(well_data, x=hist_curve, log_x=log_bool)
+            histogram.layout.template='seaborn'
+            col2_h.plotly_chart(histogram, use_container_width=True)
 
-    #Histogram Section
-    with st.beta_expander('Histograms'):
-        col1_h, col2_h = st.beta_columns(2)
-        col1_h.write('Options')
 
-        hist_curve = col1_h.selectbox('Select a Curve', columns)
-        log_option = col1_h.radio('Select Linear or Logarithmic Scale', ('Linear', 'Logarithmic'))
-        
-        if log_option == 'Linear':
-            log_bool = False
-        elif log_option == 'Logarithmic':
-            log_bool = True
-        
-        histogram = px.histogram(well_data, x=hist_curve, log_x=log_bool)
-        histogram.layout.template='seaborn'
-        col2_h.plotly_chart(histogram, use_container_width=True)
+        with st.beta_expander('Crossplot'):
+            col1, col2 = st.beta_columns(2)
+            col1.write('Options')
 
-    #Crossplot Section
-    with st.beta_expander('Crossplot'):
-        col1, col2 = st.beta_columns(2)
-        col1.write('Options')
+            xplot_x = col1.selectbox('X-Axis', columns)
+            xplot_y = col1.selectbox('Y-Axis', columns)
+            xplot_col = col1.selectbox('Color By', columns)
+            xplot_x_log = col1.radio('X Axis - Linear or Logarithmic', ('Linear', 'Logarithmic'))
+            xplot_y_log = col1.radio('Y Axis - Linear or Logarithmic', ('Linear', 'Logarithmic'))
 
-        xplot_x = col1.selectbox('X-Axis', columns)
-        xplot_y = col1.selectbox('Y-Axis', columns)
-        xplot_col = col1.selectbox('Color By', columns)
-        xplot_x_log = col1.radio('X Axis - Linear or Logarithmic', ('Linear', 'Logarithmic'))
-        xplot_y_log = col1.radio('Y Axis - Linear or Logarithmic', ('Linear', 'Logarithmic'))
+            if xplot_x_log == 'Linear':
+                xplot_x_bool = False
+            elif xplot_x_log == 'Logarithmic':
+                xplot_x_bool = True
+            
+            if xplot_y_log == 'Linear':
+                xplot_y_bool = False
+            elif xplot_y_log == 'Logarithmic':
+                xplot_y_bool = True
 
-        if xplot_x_log == 'Linear':
-            xplot_x_bool = False
-        elif xplot_x_log == 'Logarithmic':
-            xplot_x_bool = True
-        
-        if xplot_y_log == 'Linear':
-            xplot_y_bool = False
-        elif xplot_y_log == 'Logarithmic':
-            xplot_y_bool = True
+            col2.write('Crossplot')
 
-        col2.write('Crossplot')
-
-        xplot = px.scatter(well_data, x=xplot_x, y=xplot_y, color=xplot_col, log_x=xplot_x_bool, log_y=xplot_y_bool)
-        xplot.layout.template='seaborn'
-        col2.plotly_chart(xplot, use_container_width=True)
-
-        # st.write('plot')
+            xplot = px.scatter(well_data, x=xplot_x, y=xplot_y, color=xplot_col, log_x=xplot_x_bool, log_y=xplot_y_bool)
+            xplot.layout.template='seaborn'
+            col2.plotly_chart(xplot, use_container_width=True)
 
 def missing_data():
     st.title('Missing Data')
     missing_data = well_data.copy()
-
-
     missing = px.area(well_data, x='DEPTH', y='DT')
     st.plotly_chart(missing)
 
@@ -161,6 +152,6 @@ app.add_app('Home', home)
 app.add_app('Header Info', header)
 app.add_app('Data Information', raw_data)
 app.add_app('Data Visualisation', plot)
-app.add_app('Data Coverage', missing_data)
+# app.add_app('Data Coverage', missing_data)
 
 app.run()
