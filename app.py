4d035cbad8305510b4195dc026e0c065983c324f
Andy <andymcdonald1985@gmail.com>
2020-12-30 17:09:34 +0000

    Updated home page

--- app.py
+++ app.py
@@ -40,6 +40,11 @@ if las_file:
     well_data['DEPTH'] = well_data.index
 
 def home():
+    pweb = """<a href='http://andymcdonald.scot' target="_blank">http://andymcdonald.scot</a>"""
+    sm_li = """<a href='https://www.linkedin.com/in/andymcdonaldgeo/' target="_blank"><img src='https://cdn.exclaimer.com/Handbook%20Images/linkedin-icon_32x32.png'></a>"""
+    sm_tw = """<a href='https://twitter.com/geoandymcd' target="_blank"><img src='https://cdn.exclaimer.com/Handbook%20Images/twitter-icon_32x32.png'></a>"""
+    sm_med = """<a href='https://medium.com/@andymcdonaldgeo/' target="_blank"><img src='https://cdn.exclaimer.com/Handbook%20Images/Medium_32.png'></a>"""
+
     st.title('LAS Data Explorer - Version 0.1')
     st.write('## Welcome to the LAS Data Explorer')
     st.write('### Created by Andy McDonald')
@@ -52,13 +57,10 @@ def home():
     st.write('**Data Information Info:** Information about the curves contained within the LAS file, including names, statisics and raw data values.')
     st.write('**Data Visualisation:** Visualisation tools to view las file data on a log plot, crossplot and histogram.')
     st.write('## Get in Touch')
-    githublink = """<a href='https://github.com/andymcdgeo/las_explorer' target="_blank">https://github.com/andymcdgeo/las_explorer</a>"""
-    st.write(f'\n\nCheck out the GitHub Repo at: {githublink}. If you find any bugs or have suggestions, please open a new issue and I will look into it.', unsafe_allow_html=True)
-    st.write('\nIf you want to get in touch, you can find me on Social Media.')
+    st.write(f'\nIf you want to get in touch, you can find me on Social Media at the links below or visit my website at: {pweb}.', unsafe_allow_html=True)
+
     
-    sm_li = """<a href='https://github.com/andymcdgeo/las_explorer' target="_blank"><img src='/graphics/linkedin.png'></a>"""
-    
-    st.write(f'{sm_li}', unsafe_allow_html=True)
+    st.write(f'{sm_li}  {sm_med}  {sm_tw}', unsafe_allow_html=True)
 
     st.write('## Source Code, Bugs, Feature Requests')
     githublink = """<a href='https://github.com/andymcdgeo/las_explorer' target="_blank">https://github.com/andymcdgeo/las_explorer</a>"""
